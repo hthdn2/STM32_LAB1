21c21f84f854caf5b3eee3db695bc7cff8e6e4bd
LAPTOP-93E61UNQ\ADMIN <dinhtam265@gmail.com>
2023-09-25 12:21:24 +0700

    Update for Ex6

--- LAB1/Core/Src/main.c
+++ LAB1/Core/Src/main.c
@@ -54,7 +54,47 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+void setNumberOnClock(int num){
+	switch(num){
+	case 1:
+		HAL_GPIO_TogglePin(LED_1_GPIO_Port, LED_1_Pin);
+		break;
+	case 2:
+		HAL_GPIO_TogglePin(LED_2_GPIO_Port, LED_2_Pin);
+		break;
+	case 3:
+		HAL_GPIO_TogglePin(LED_3_GPIO_Port, LED_3_Pin);
+		break;
+	case 4:
+		HAL_GPIO_TogglePin(LED_4_GPIO_Port, LED_4_Pin);
+		break;
+	case 5:
+		HAL_GPIO_TogglePin(LED_5_GPIO_Port, LED_5_Pin);
+		break;
+	case 6:
+		HAL_GPIO_TogglePin(LED_6_GPIO_Port, LED_6_Pin);
+		break;
+	case 7:
+		HAL_GPIO_TogglePin(LED_7_GPIO_Port, LED_7_Pin);
+		break;
+	case 8:
+		HAL_GPIO_TogglePin(LED_8_GPIO_Port, LED_8_Pin);
+		break;
+	case 9:
+		HAL_GPIO_TogglePin(LED_9_GPIO_Port, LED_9_Pin);
+		break;
+	case 10:
+		HAL_GPIO_TogglePin(LED_10_GPIO_Port, LED_10_Pin);
+		break;
+	case 11:
+		HAL_GPIO_TogglePin(LED_11_GPIO_Port, LED_11_Pin);
+		break;
+	case 0:
+		HAL_GPIO_TogglePin(LED_12_GPIO_Port, LED_12_Pin);
+		break;
+	default:
+	}
+}
 /* USER CODE END 0 */
 
 /**
@@ -91,8 +131,12 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  int counter = 0;
   while (1)
   {
+	  if (counter <= 11) setNumberOnClock(counter++);
+	  else counter = 1;
+	  HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
